59d488e40a38383844dad5e36bc68539b8c8472c
KateSebring <ihatesuperman99@gmail.com>
2026-06-10 12:51:05 -0400

    Establish a math class that currently holds four basic functions

---
+++ lib/math.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Math  {
+  double add(double val1, double val2) {
+    return val1 + val2;
+  }
+
+  double subtract(double val1, double val2) {
+    return val1 - val2;
+  }
+
+  double multiply(double val1, double val2) {
+    return val1 * val2;
+  }
+
+  double divide(double val1, double val2) {
+    return val1 / val2;
+  }
+}
