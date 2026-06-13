2aca74c2fee16b45196bd9ce0d226946d24ca940
KateSebring <ihatesuperman99@gmail.com>
2026-06-13 11:42:31 -0400

    Implement very simple calculations with two single-digit numbers

--- lib/main.dart
+++ lib/main.dart
@@ -17,8 +17,11 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   void updateDisplayField(String value) {
+    // allow longer fields eventually
     setState(() {
-      displayField += value;
+      if(displayField.length < 3) {
+        displayField += value;
+      }
     });
   }
 
@@ -34,6 +37,43 @@ class _MyAppState extends State<MyApp> {
         displayField = displayField.substring(0, displayField.length - 1);
       }
     });
+  }
+
+  void calculateTotal(String input) {
+    Math calculations = Math();
+    String symbol = input[1];
+    // check if middle symbol is valid
+    // check if first and last symbols are valid
+    // parse out the symbol
+    if(num.tryParse(input[0]) != null && num.tryParse(input[2]) != null) {
+      double val1 = double.parse(input[0]);
+      double val2 = double.parse(input[2]);
+      // make this not display decimal point for three main functions
+      switch(symbol) {
+        case '+':
+          setState(() {
+            displayField = calculations.add(val1, val2).toString();
+          });
+        case '-':
+          setState(() {
+            displayField = calculations.subtract(val1, val2).toString();
+          }); 
+        case '*':
+          setState(() {
+            displayField = calculations.multiply(val1, val2).toString();
+          });
+        case '/':
+          if(val2 == 0) {
+            setState(() {
+              displayField = "ERROR: cannot divide by zero.";
+            });
+          } else {
+            setState(() {
+              displayField = calculations.divide(val1, val2).toString();
+            });
+          }
+      }
+    }
   }
   // This widget is the root of your application.
   @override
@@ -195,7 +235,7 @@ class _MyAppState extends State<MyApp> {
                       child: CalcButton(
                         buttonText: '=', 
                         onPressed: (value) {
-                          updateDisplayField(value);
+                          calculateTotal(displayField);
                         },
                       ),
                     ),
